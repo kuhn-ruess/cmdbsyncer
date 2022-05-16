e2413b807fb3e4397ccf53f29c01aa3209f9e216
Bastian Kuhn <mail@bastian-kuhn.de>
2022-05-16 10:45:23 +0200

    Account View: Directly show custom Attributes in Table view

--- application/views/account.py
+++ application/views/account.py
@@ -3,6 +3,15 @@ Account Model View
 """
 from flask_login import current_user
 from application.views.default import DefaultModelView
+from markupsafe import Markup
+
+
+def _render_custom_data(_view, _context, model, name):
+    html = "<table>"
+    for entry in model.custom_fields:
+        html += f"<tr><td>{entry.name}</td><td>{entry.value}</td></tr>"
+    html += "</table>"
+    return Markup(html)
 
 class AccountModelView(DefaultModelView):
     """
@@ -12,6 +21,15 @@ class AccountModelView(DefaultModelView):
        'name',
        'enabled',
     )
+
+
+    column_labels = {
+        'password': 'Attributes',
+    }
+
+    column_formatters = {
+        'password': _render_custom_data,
+    }
 
     def is_accessible(self):
         """ Overwrite """
