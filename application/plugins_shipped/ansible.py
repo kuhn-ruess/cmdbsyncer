94e7352308c70448ed3f10abc721f66e255e1708
Bastian Kuhn <mail@bastian-kuhn.de>
2022-08-10 11:27:55 +0200

    Adde Debug Mode for Ansible Actions

--- application/plugins_shipped/ansible.py
+++ application/plugins_shipped/ansible.py
@@ -4,12 +4,15 @@ Ansible Inventory Modul
 """
 #pylint: disable=too-many-arguments
 import json
+from pprint import pprint
 import click
+
 
 from application import app
 from application.models.host import Host
 from application.helpers.get_ansible_action import GetAnsibleAction
 from application.helpers.get_label import GetLabel
+from application.helpers.debug import ColorCodes
 
 
 from mongoengine.errors import DoesNotExist
@@ -22,7 +25,7 @@ def get_rule_helper():
     return helper
 
 
-@app.cli.command('run_cmk2_inventory')
+@app.cli.command('ansible-inventory')
 @click.argument('account')
 def run_cmk2_inventory(account):
     """
@@ -36,16 +39,40 @@ def run_cmk2_inventory(account):
         labels, _ = label_helper.filter_labels(db_host.get_labels())
         ansible_rules = action_helper.get_action(db_host, labels)
         print(f"{db_host.hostname}: {ansible_rules}")
+        print(account)
+
+
+
+@app.cli.command('ansible-debug')
+@click.argument("host")
+def debug_ansible_rules(host):
+    """
+    Debug Ansible Rules for Host
+    """
+    action_helper = GetAnsibleAction(debug=True)
+    label_helper = GetLabel()
+    db_host = Host.objects.get(hostname=host)
+    labels, _ = label_helper.filter_labels(db_host.get_labels())
+    ansible_rules = action_helper.get_action(db_host, labels)
+    print()
+    print(f"{ColorCodes.HEADER} ***** Final Outcomes ***** {ColorCodes.ENDC}")
+    print(f"{ColorCodes.UNDERLINE} Labels in DB {ColorCodes.ENDC}")
+    pprint(db_host.get_labels())
+    print(f"{ColorCodes.UNDERLINE}Labels after Filter {ColorCodes.ENDC}")
+    pprint(labels)
+    print(f"{ColorCodes.UNDERLINE}Outcomes based on Ansible Rules {ColorCodes.ENDC}")
+    pprint(ansible_rules)
 
 
 
 @app.cli.command('ansible')
 @click.option("--list", is_flag=True)
 @click.option("--host")
-def maintenance(list, host):
+def maintenance(list, host): #pylint: disable=redefined-builtin
     """Return JSON Inventory Data for Ansible"""
     action_helper = GetAnsibleAction()
     label_helper = GetLabel()
+    #pylint: disable=no-else-return
     if list:
         data = {
             '_meta': {
@@ -83,3 +110,5 @@ def maintenance(list, host):
             inventory.update(ansible_rules['vars'])
         print(json.dumps(inventory))
         return True
+    print("Params missing")
+    return False
