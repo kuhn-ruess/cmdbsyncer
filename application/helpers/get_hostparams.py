018e24331cafb2739225f51d573c41c154b82694
Bastian Kuhn <mail@bastian-kuhn.de>
2022-10-18 11:38:08 +0200

    Fixed Custom Params merge

--- application/helpers/get_hostparams.py
+++ application/helpers/get_hostparams.py
@@ -30,6 +30,7 @@ class GetHostParams(): # pylint: disable=too-few-public-methods
                 outcome['ignore_host'] = True
             elif param['type'] == "add_custom_label":
                 outcome.setdefault('custom_labels', {})
+                print(param['value'])
                 outcome['custom_labels'][param['name']] = param['value']
         return outcome
 
@@ -40,12 +41,14 @@ class GetHostParams(): # pylint: disable=too-few-public-methods
         """
 
         # First rule Match
-        outcomes = {}
+        outcomes = {'custom_labels': {}}
         for rule in self.rules:
             for condtion in rule['conditions']:
                 cond_hostname = condtion['hostname']
                 if match(hostname, cond_hostname, condtion['match'], condtion['match_negate']):
-                    outcomes.update(self._convert_params(rule['params']))
+                    new_outcome = self._convert_params(rule['params'])
+                    new_outcome.get('custom_labels', {}).update(outcomes['custom_labels'])
+                    outcomes.update(new_outcome)
         return outcomes
 
 
