03886bacf3286c912fcaf4184e8063fbb0216819
Bastian Kuhn <mail@bastian-kuhn.de>
2022-11-02 12:36:04 +0100

    Functions to import or inventorize CSV Files

--- application/plugins_shipped/csv.py
+++ application/plugins_shipped/csv.py
@@ -7,6 +7,7 @@ import csv
 import click
 from application import app
 from application.models.host import Host
+from application.helpers.debug import ColorCodes
 
 @app.cli.group(name='csv')
 def cli_csv():
@@ -30,3 +31,51 @@ def compare_csv(csv_path, delimiter, hostname_field):
             hostname = row[hostname_field]
             if hostname not in host_list:
                 print(hostname)
+
+@cli_csv.command('import_hosts')
+@click.argument("csv_path")
+@click.argument("delimiter", default=';')
+@click.argument("hostname_field", default='host')
+def import_csv(csv_path, delimiter, hostname_field):
+    """
+    Import and Maintane Hosts from given CSV
+    """
+    #pylint: disable=no-member, consider-using-generator
+    filename = csv_path.split('/')[-1]
+    print(f"{ColorCodes.OKBLUE}Started {ColorCodes.ENDC}"\
+          f"{ColorCodes.UNDERLINE}{filename}{ColorCodes.ENDC}")
+    with open(csv_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=delimiter)
+        for row in reader:
+            hostname = row[hostname_field]
+            print(f" {ColorCodes.OKGREEN}** {ColorCodes.ENDC} Update {hostname}")
+            host_obj = Host.get_host(hostname)
+            del row[hostname_field]
+            host_obj.set_labels(row)
+            host_obj.set_import_seen()
+            host_obj.set_account(f"csv_{filename}", filename)
+            host_obj.save()
+
+@cli_csv.command('inventorize_hosts')
+@click.argument("csv_path")
+@click.argument("delimiter", default=';')
+@click.argument("hostname_field", default='host')
+@click.argument("key", default='csv')
+def inventorize_csv(csv_path, delimiter, hostname_field, key):
+    """
+    Do inventory for fields in given csv
+    """
+    #pylint: disable=no-member, consider-using-generator
+    filename = csv_path.split('/')[-1]
+    print(f"{ColorCodes.OKBLUE}Started {ColorCodes.ENDC}"\
+          f"{ColorCodes.UNDERLINE}{filename}{ColorCodes.ENDC}")
+    with open(csv_path, newline='', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile, delimiter=delimiter)
+        for row in reader:
+            hostname = row[hostname_field]
+            print(f" {ColorCodes.OKGREEN}** {ColorCodes.ENDC} Update {hostname}")
+            host_obj = Host.get_host(hostname, False)
+            if host_obj:
+                del row[hostname_field]
+                host_obj.update_inventory(key, {f"{key}_{x}":y for x,y in row.items()})
+                host_obj.save()
