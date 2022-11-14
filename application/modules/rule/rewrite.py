884a51dc18ab8bef23b0f3fb7f59ca1ea84909bb
Bastian Kuhn <mail@bastian-kuhn.de>
2022-11-14 07:43:56 +0100

    Netbox: Inital Interface Export based on Cisco DNA

--- application/modules/rule/rewrite.py
+++ application/modules/rule/rewrite.py
@@ -2,6 +2,7 @@
 """
 Rewrite
 """
+import re
 from application.modules.rule.rule import Rule
 
 class Rewrite(Rule):# pylint: disable=too-few-public-methods
@@ -10,6 +11,7 @@ class Rewrite(Rule):# pylint: disable=too-few-public-methods
     """
 
     name = "Rewrite"
+    rewrite_cache = {}
 
     def add_outcomes(self, rule_outcomes, outcomes):
         """
@@ -19,7 +21,18 @@ class Rewrite(Rule):# pylint: disable=too-few-public-methods
         for outcome in rule_outcomes:
             attribute_name = outcome['old_attribute_name']
             new_attribute_name = outcome['new_attribute_name']
-            if self.attributes.get(attribute_name):
-                outcomes[f'add_{new_attribute_name}'] = self.attributes[attribute_name]
-                outcomes[f'del_{attribute_name}'] = True
+            if attribute_name.startswith('~'):
+                pattern = attribute_name[1:]
+                # Can may be simplified since i heared Python
+                # Handles the cache anyway
+                if not self.rewrite_cache.get(pattern):
+                    self.rewrite_cache[pattern] = re.compile(pattern)
+                for key, value in self.attributes.items():
+                    if self.rewrite_cache[pattern].match(key):
+                        outcomes[f'add_{new_attribute_name}'] = value
+                        outcomes[f'del_{key}'] = True
+            else:
+                if self.attributes.get(attribute_name):
+                    outcomes[f'add_{new_attribute_name}'] = self.attributes[attribute_name]
+                    outcomes[f'del_{attribute_name}'] = True
         return outcomes
