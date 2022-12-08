f5aaad8471c68f734b6145a504e9e056320d9447
Bastian Kuhn <mail@bastian-kuhn.de>
2022-12-08 18:04:24 +0100

    updated version

--- application/__init__.py
+++ application/__init__.py
@@ -14,7 +14,7 @@ from flask_bootstrap import Bootstrap
 from flask_mongoengine import MongoEngine
 
 
-VERSION = '3.0.0-pre2'
+VERSION = '3.0.0-pre3'
 
 app = Flask(__name__)
 env = os.environ.get('config')
