53cc96ffc90101069506f9381432dcbb146db71d
Bastian Kuhn <mail@bastian-kuhn.de>
2023-01-31 16:49:21 +0100

    Fixed missing colors for Conditions

--- application/modules/checkmk/views.py
+++ application/modules/checkmk/views.py
@@ -111,6 +111,23 @@ class CheckmkMngmtRuleView(RuleModelView):
     Custom Group Model View
     """
 
+    form_subdocuments = {
+        'conditions': {
+            'form_subdocuments' : {
+                None: {
+                    'form_widget_args': {
+                        'hostname_match': { 'style': 'background-color: #2EFE9A' },
+                        'hostname': { 'style': 'background-color: #2EFE9A' },
+                        'tag_match': { 'style': 'background-color: #81DAF5' },
+                        'tag': { 'style': 'background-color: #81DAF5' },
+                        'value_match': { 'style': 'background-color: #81DAF5' },
+                        'value': { 'style': 'background-color: #81DAF5' },
+                    },
+                }
+            }
+        }
+    }
+
     def __init__(self, model, **kwargs):
         """
         Update elements
