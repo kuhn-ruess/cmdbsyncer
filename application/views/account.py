035bbb73cbbd2d7d9215695b469c5fb670be4be5
Bastian Kuhn <mail@bastian-kuhn.de>
2023-03-07 09:52:36 +0100

    Account cleanups

--- application/views/account.py
+++ application/views/account.py
@@ -1,12 +1,16 @@
 """
 Account Model View
 """
-from flask_login import current_user
-from application.views.default import DefaultModelView
 from markupsafe import Markup
+from flask_login import current_user
+from wtforms import StringField
+from application.views.default import DefaultModelView
 
 
-def _render_custom_data(_view, _context, model, name):
+def _render_custom_data(_view, _context, model, _name):
+    """
+    Render for detail table
+    """
     html = "<table width=100%>"
     for entry in model.custom_fields:
         html += f"<tr><td>{entry.name}</td><td>{entry.value}</td></tr>"
@@ -30,6 +34,18 @@ class AccountModelView(DefaultModelView):
     column_formatters = {
         'password': _render_custom_data,
     }
+
+    form_overrides = {
+        'name': StringField,
+        'password': StringField,
+        'address': StringField,
+        'username': StringField,
+    }
+
+    form_widget_args = {
+        'password': {'autocomplete': 'new-password' },
+    }
+
 
     def is_accessible(self):
         """ Overwrite """
