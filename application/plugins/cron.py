9085147f58d08adb0bc25a93245c6c22ff0dfb31
Bastian Kuhn <mail@bastian-kuhn.de>
2023-03-17 15:35:33 +0100

    State Handling for Cronjobs

--- application/plugins/cron.py
+++ application/plugins/cron.py
@@ -4,6 +4,7 @@ CronJobs
 #pylint: disable=too-many-arguments
 import json
 import click
+import os
 from datetime import datetime, timedelta
 from application import app, cron_register
 from application.modules.debug import ColorCodes
@@ -51,10 +52,6 @@ def next_run(interval):
     if hours:
         return now + timedelta(hours=hours)
 
-
-
-
-
 @_cli_cron.command('run_jobs')
 @click.option("-v", default=False)
 def run_jobs(v):
@@ -66,16 +63,22 @@ def run_jobs(v):
         stats = get_stats(job.name)
 
         # Add Time Delta of 1min to get jobs which never run before
-        if stats.next_run <= now+timedelta(minutes=1):
+        if not stats.is_running and stats.next_run <= now+timedelta(minutes=1):
             print('-------------------------------------------------------------')
             print(f"{ColorCodes.HEADER} Running Group {job.name} {ColorCodes.ENDC}")
+            stats.is_running = True
+            stats.last_start = now
+            stats.save()
             for task in job.jobs:
                 print(f"{ColorCodes.UNDERLINE}{ColorCodes.OKBLUE}Task: {task.name} {ColorCodes.ENDC}")
+                stats.last_message = f"{now}: Started {task.name} (PID: {os.getpid()})"
+                stats.save()
                 try:
                     cron_register[task.command](account=task.account.name)
                 except Exception as error_obj:
                     print(Exception, error_obj)
 
-            stats.last_run = now
+            stats.last_ended = datetime.now()
             stats.next_run = next_run(job.interval)
+            stats.is_running = False
             stats.save()
--- application/views/cron.py
+++ application/views/cron.py
@@ -2,19 +2,15 @@
 Cron Model View
 """
 # pylint: disable=too-few-public-methods
-import re
-from mongoengine.errors import DoesNotExist
+from datetime import datetime
 from flask_login import current_user
-from flask_admin.actions import action
-from flask_admin.contrib.mongoengine.filters import BaseMongoEngineFilter
-
-from flask import flash
-from flask import Markup
 
 from application.views.default import DefaultModelView
-from application.models.host import Host
-from application.models.config import Config
 
+
+def format_date(v, c, m, p):
+    """ Format Date Field"""
+    return datetime.strftime(getattr(m,p), "%d.%m.%Y %H:%M")
 
 
 
@@ -32,6 +28,12 @@ class CronStatsView(DefaultModelView):
     page_size = 50
     can_set_page_size = True
 
+    column_formatters = {
+        'next_run': format_date,
+        'last_run': format_date,
+        'last_start': format_date,
+        'last_ended': format_date,
+    }
     def is_accessible(self):
         """ Overwrite """
         return current_user.is_authenticated and current_user.has_right('cron')
