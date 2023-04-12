9048634b45ead8f28837faa2ee7b1674c71035e0
Bastian Kuhn <mail@bastian-kuhn.de>
2023-04-12 14:16:12 +0200

    Delete Cache in GUI and CLI

--- application/plugins/maintenance.py
+++ application/plugins/maintenance.py
@@ -56,6 +56,19 @@ def cli_maintenance(days):
     """
     maintenance(days)
 #.
+#   .-- Command: Delete Caches
+@_cli_sys.command('delete_cache')
+def delete_cache():
+    """
+    Delete object Cache
+    """
+    print(f"{ColorCodes.HEADER} ***** Delete Cache ***** {ColorCodes.ENDC}")
+    for host in Host.objects():
+        host.cache = {}
+        host.save()
+    print(f"{ColorCodes.OKGREEN}  ** {ColorCodes.ENDC}Done")
+
+#.
 #   .-- Command: Delete all Hosts
 @_cli_sys.command('delete_all_hosts')
 def delete_all_hosts():
