44cbad00996898848d9a3bd97cf3268f2656e7c3
Bastian Kuhn <mail@bastian-kuhn.de>
2023-07-14 14:55:26 +0200

    Introduce basics of universal rule import/export

--- application/plugins/rule_import_export.py
+++ application/plugins/rule_import_export.py
@@ -4,42 +4,60 @@ Rule Import/ Export
 #pylint: disable=too-many-arguments
 import json
 import click
+import importlib
+from ast import literal_eval
+
 from mongoengine.errors import NotUniqueError
-
 from application import app
-
-from application.modules.ansible.models import AnsibleFilterRule, AnsibleRewriteAttributesRule, \
-                                               AnsibleCustomVariablesRule
 
 from application.modules.rule.models import CustomAttribute, FullCondition, FilterAction
 
+enabled_rules = {
+    'ansible_customvars': ('application.modules.ansible.models', 'AnsibleCustomVariablesRule')
+}
 
 
-def export_ansible():
+def export_rules_from_model(rule_type):
     """
-    @TODO: Move to Modules
+    Export Given Rulesets
     """
-    for db_rule in AnsibleCustomVariablesRule.objects():
-        print(db_rule)
+    model = importlib.import_module(enabled_rules[rule_type][0])
+    for db_rule in getattr(model, enabled_rules[rule_type][1]).objects():
+        yield db_rule.to_json()
+
+
+def import_rules_to_model(rule_type, json_raw):
+    """
+    Import Rules to Model
+    """
+    model = importlib.import_module(enabled_rules[rule_type][0])
+    for json_rule_raw in json_raw:
+        json_dict = json.loads(json_rule_raw)
+        print(f"* Import {json_dict['name']}")
+        db_ref = getattr(model, enabled_rules[rule_type][1])()
+        new = db_ref.from_json(json.dumps(json_dict))
+        try:
+            new.save(force_insert=True)
+        except NotUniqueError:
+            print("   Already existed")
 
 @app.cli.group(name='rules')
 def cli_rules():
     """Rule related commands"""
 
 @cli_rules.command('export_rules')
-@click.argument("rule_category")
-def export_rules(rule_category):
+@click.argument("rule_type", default="")
+def export_rules(rule_type):
     """
     Export Rules by Category
     """
-    categories = {
-        'ansible': export_ansible,
-    }
-    if rule_category.lower() in categories:
-        categories[rule_category.lower()]()
+    if rule_type.lower() in enabled_rules:
+        rules = list(export_rules_from_model(rule_type))
+        print({'rule_type': rule_type,
+               'rules_json': rules})
     else:
         print("Ruletype not supported")
-        print(f"Currently supported: {','.join(categories.keys())}")
+        print(f"Currently supported: {','.join(enabled_rules.keys())}")
 
 @cli_rules.command('import_rules')
 @click.argument("rulefile_path")
@@ -47,36 +65,12 @@ def import_rules(rulefile_path):
     """
     Import Rules into the CMDB Syncer
     """
-    supported_rules = {
-        'AnsibleCustomVariablesRule': AnsibleCustomVariablesRule
-    }
     with open(rulefile_path, newline='', encoding='utf-8') as rulefile:
-        for json_rule in json.load(rulefile):
-            print(f"Import: {json_rule['rule_name']}")
-            new_rule = supported_rules[json_rule['rule_type']]()
-            new_rule.name = json_rule['rule_name']
-            new_rule.condition_typ = json_rule['condition_typ']
-            new_rule.enabled = False
-            new_rule.sort_field = json_rule['sort_field']
-            conditions = []
-            for cond in json_rule['conditions']:
-                if cond['type'] == 'label':
-                    condition = FullCondition()
-                    condition.match_type = 'tag'
-                    condition.tag_match = cond['tag'][0]
-                    condition.tag = cond['tag'][1]
-                    condition.value_match = cond['value'][0]
-                    condition.value = cond['value'][1]
-                    conditions.append(condition)
-            new_rule.conditions = conditions
-            outcomes = []
-            for out in json_rule['outcomes']:
-                attribute = CustomAttribute()
-                attribute.attribute_name = out[0]
-                attribute.attribute_value = out[1]
-                outcomes.append(attribute)
-            new_rule.outcomes = outcomes
-            try:
-                new_rule.save()
-            except NotUniqueError:
-                print(" -- Already existed")
+        rules_obj = literal_eval(rulefile.read())
+        rule_type = rules_obj['rule_type']
+        rule_json_field = rules_obj['rules_json']
+        if rule_type not in enabled_rules:
+            print("Ruletype not supported")
+            print(f"Currently supported: {','.join(enabled_rules.keys())}")
+        else:
+            import_rules_to_model(rule_type, rule_json_field)
