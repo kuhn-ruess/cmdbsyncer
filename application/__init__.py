cb59cb38eee661fede6b6c5c7fc6e54b85b37f2b
Bastian Kuhn <mail@bastian-kuhn.de>
2023-08-03 19:32:25 +0200

    Updated Version

--- application/__init__.py
+++ application/__init__.py
@@ -16,7 +16,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.2.4'
+VERSION = '3.2.5'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
