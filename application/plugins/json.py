a1d2567d533b8006d3dc9ca3af43c93a2c46e924
Bastian Kuhn <mail@bastian-kuhn.de>
2023-08-24 13:57:19 +0200

    Fixed mkdocs

--- application/plugins/json.py
+++ application/plugins/json.py
@@ -1,13 +1,18 @@
 """
 JSON Plugin
 """
-#pylint: disable=too-many-arguments
+#pylint: disable=too-many-arguments, logging-fstring-interpolation
 import json
 import click
-from application import app
+
+import requests
+from requests.auth import HTTPBasicAuth
+
+from application import app, logger
 from application.models.host import Host
 from application.modules.debug import ColorCodes
 from application.helpers.get_account import get_account_by_name
+from application.helpers.cron import register_cronjob
 
 @app.cli.group(name='json')
 def _cli_json():
@@ -51,3 +56,49 @@ def import_hosts(json_path, hostname_field, account):
 
             if do_save:
                 host_obj.save()
+
+@app.cli.group(name='rest')
+def _cli_rest():
+    """REST API related Commands"""
+
+
+def import_hosts_json(account):
+    """
+    Import Hosts from REST API
+    """
+    account = get_account_by_name(account)
+    headers = {}
+    auth = None
+    if account.get('request_headers'):
+        headers = dict(account['request_headers'])
+
+    if account.get('use_auth_basic'):
+        auth = HTTPBasicAuth(account['username'], account['password'])
+
+    logger.debug(f"Auth: {auth}")
+
+    response = requests.post(account['address'], headers=headers, auth=auth, timeout=30)
+    data = response.json()
+    logger.debug(f"Response: {data}")
+
+    for entry in data[account['data_key']]:
+        hostname = entry[account['hostname_field']]
+        del entry[account['hostname_field']]
+
+        host_obj = Host.get_host(hostname)
+        host_obj.update_host(entry)
+
+        do_save = host_obj.set_account(account_dict=account)
+
+        if do_save:
+            host_obj.save()
+
+@_cli_rest.command('import_hosts')
+@click.argument("account")
+def cli_import_hosts_json(account):
+    """
+    Import Json direct from Rest API
+    """
+    return import_hosts_json(account)
+
+register_cronjob('REST API: Import Hosts', import_hosts_json)
