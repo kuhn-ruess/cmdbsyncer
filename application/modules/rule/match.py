ecd401f6427bbf70f94b43c0cd03e1b3dfd9f994
Bastian Kuhn <mail@bastian-kuhn.de>
2023-08-28 14:42:35 +0200

    Better Error messages on match errors

--- application/modules/rule/match.py
+++ application/modules/rule/match.py
@@ -24,67 +24,70 @@ def match(value, needle, condition, negate=False):
     """
     Check for Match for given params
     """
-    # pylint: disable=too-many-branches, too-many-return-statements
-    if condition == 'ignore' and negate:
-        # In case that rule ignore is negate, than the condition simply not match
-        return False
+    try:
+        # pylint: disable=too-many-branches, too-many-return-statements
+        if condition == 'ignore' and negate:
+            # In case that rule ignore is negate, than the condition simply not match
+            return False
 
-    if condition == 'ignore':
-        return True
+        if condition == 'ignore':
+            return True
 
-    if condition == 'bool':
-        value = make_bool(value)
-        needle = make_bool(needle)
-    else:
-        value = str(value).lower()
-        needle = str(needle).lower()
+        if condition == 'bool':
+            value = make_bool(value)
+            needle = make_bool(needle)
+        else:
+            value = str(value).lower()
+            needle = str(needle).lower()
 
-    if negate:
+        if negate:
+            if condition == 'equal':
+                if value != needle:
+                    return True
+            elif condition == 'in':
+                if needle not in value:
+                    return True
+            elif condition == 'in_list':
+                if value not in [x.strip() for x in needle.split(',')]:
+                    return True
+            elif condition == 'swith':
+                if not value.startswith(needle):
+                    return True
+            elif condition == 'ewith':
+                if not value.endswith(needle):
+                    return True
+            elif condition == 'regex':
+                pattern = re.compile(needle) #@TODO Cache
+                if not pattern.match(value):
+                    return True
+            elif condition == 'bool':
+                if needle != value:
+                    return True
+
+            return False
+
         if condition == 'equal':
-            if value != needle:
+            if value == needle:
                 return True
         elif condition == 'in':
-            if needle not in value:
+            if needle in value:
                 return True
         elif condition == 'in_list':
-            if value not in [x.strip() for x in needle.split(',')]:
+            if value in [x.strip() for x in needle.split(',')]:
                 return True
         elif condition == 'swith':
-            if not value.startswith(needle):
+            if value.startswith(needle):
                 return True
         elif condition == 'ewith':
-            if not value.endswith(needle):
+            if value.endswith(needle):
                 return True
         elif condition == 'regex':
-            pattern = re.compile(needle) #@TODO Cache
-            if not pattern.match(value):
+            pattern = re.compile(needle)
+            if pattern.match(value):
                 return True
         elif condition == 'bool':
-            if needle != value:
+            if needle == value:
                 return True
-
         return False
-
-    if condition == 'equal':
-        if value == needle:
-            return True
-    elif condition == 'in':
-        if needle in value:
-            return True
-    elif condition == 'in_list':
-        if value in [x.strip() for x in needle.split(',')]:
-            return True
-    elif condition == 'swith':
-        if value.startswith(needle):
-            return True
-    elif condition == 'ewith':
-        if value.endswith(needle):
-            return True
-    elif condition == 'regex':
-        pattern = re.compile(needle)
-        if pattern.match(value):
-            return True
-    elif condition == 'bool':
-        if needle == value:
-            return True
-    return False
+    except Exception as error:
+        raise Exception(f"Condition Failed: {condition}, Value: {value}, Needed: {needle}. Hint: {error}")
