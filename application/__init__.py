7690dbe107c7ca5397c5b2bca389c86353d73cb5
Bastian Kuhn <mail@bastian-kuhn.de>
2023-09-05 14:15:42 +0200

    Checkmk: Added Support for diffrent Group Titles for Objects in Checkmk

--- application/__init__.py
+++ application/__init__.py
@@ -16,7 +16,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.3.0'
+VERSION = '3.3.1'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
