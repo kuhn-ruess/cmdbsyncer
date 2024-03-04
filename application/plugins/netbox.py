e746e4e2bf261f1ef57618fd21523ed28346e572
Bastian Kuhn <mail@bastian-kuhn.de>
2024-03-04 11:56:23 +0100

    Fixed: debug_host now cleans Custom Attributes cache

--- application/plugins/netbox.py
+++ application/plugins/netbox.py
@@ -117,6 +117,7 @@ def netbox_host_debug(hostname):
         for key in list(db_host.cache.keys()):
             if key.lower().startswith('netbox'):
                 del db_host.cache[key]
+        del db_host.cache['CustomAttributeRule']
         db_host.save()
     except DoesNotExist:
         print(f"{ColorCodes.FAIL}Host not Found{ColorCodes.ENDC}")
