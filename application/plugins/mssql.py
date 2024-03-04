42d5176c6e10c02bd08be8e5425a350173acf541
Bastian Kuhn <mail@bastian-kuhn.de>
2024-03-04 15:05:26 +0100

    FIX: MSSQL Plugin requirements now part of the extras

--- application/plugins/mssql.py
+++ application/plugins/mssql.py
@@ -20,36 +20,39 @@ def mssql_import(account):
     """
     Mssql Import
     """
-    config = get_account_by_name(account)
+    try:
+        config = get_account_by_name(account)
 
-    print(f"{ColorCodes.OKBLUE}Started {ColorCodes.ENDC} with account "\
-          f"{ColorCodes.UNDERLINE}{config['name']}{ColorCodes.ENDC}")
+        print(f"{ColorCodes.OKBLUE}Started {ColorCodes.ENDC} with account "\
+              f"{ColorCodes.UNDERLINE}{config['name']}{ColorCodes.ENDC}")
 
-    serverport = sqlserverport.lookup(config['address'], config['instance'])
-    server = f'{config["address"]},{serverport}'
-    connect_str = f'DRIVER={{{config["driver"]}}};SERVER={server};DATABASE={config["database"]};UID={config["username"]};PWD={config["password"]};TrustServerCertificate=YES'
-    cnxn = pyodbc.connect(connect_str)
-    cursor = cnxn.cursor()
-    query = f"select {config['fields']} from {config['table']}"
-    if 'where' in config and config['where']:
-        query += f"WHERE {config['where']}"
-    query += ";"
-    cursor.execute(query)
-    rows = cursor.fetchall()
-    for row in rows:
-        labels=dict(zip(config['fields'].split(","),row))
-        hostname = labels[config['hostname_field']].strip().lower()
-        if 'rewrite_hostname' in config and config['rewrite_hostname']:
-            hostname = Host.rewrite_hostname(hostname, config['rewrite_hostname'], labels)
-        print(f" {ColorCodes.OKGREEN}* {ColorCodes.ENDC} Check {hostname}")
-        del labels[config['hostname_field']]
-        host_obj = Host.get_host(hostname)
-        host_obj.update_host(labels)
-        do_save=host_obj.set_account(account_dict=config)
-        if do_save:
-            host_obj.save()
-        else:
-            print(f" {ColorCodes.WARNING} * {ColorCodes.ENDC} Managed by diffrent master")
+        serverport = sqlserverport.lookup(config['address'], config['instance'])
+        server = f'{config["address"]},{serverport}'
+        connect_str = f'DRIVER={{{config["driver"]}}};SERVER={server};DATABASE={config["database"]};UID={config["username"]};PWD={config["password"]};TrustServerCertificate=YES'
+        cnxn = pyodbc.connect(connect_str)
+        cursor = cnxn.cursor()
+        query = f"select {config['fields']} from {config['table']}"
+        if 'where' in config and config['where']:
+            query += f"WHERE {config['where']}"
+        query += ";"
+        cursor.execute(query)
+        rows = cursor.fetchall()
+        for row in rows:
+            labels=dict(zip(config['fields'].split(","),row))
+            hostname = labels[config['hostname_field']].strip().lower()
+            if 'rewrite_hostname' in config and config['rewrite_hostname']:
+                hostname = Host.rewrite_hostname(hostname, config['rewrite_hostname'], labels)
+            print(f" {ColorCodes.OKGREEN}* {ColorCodes.ENDC} Check {hostname}")
+            del labels[config['hostname_field']]
+            host_obj = Host.get_host(hostname)
+            host_obj.update_host(labels)
+            do_save=host_obj.set_account(account_dict=config)
+            if do_save:
+                host_obj.save()
+            else:
+                print(f" {ColorCodes.WARNING} * {ColorCodes.ENDC} Managed by diffrent master")
+    except NameError:
+        print("EXCEPTION: Missing requirements, pypyodbc or sqlserverport")
 
 @cli_mssql.command('import_hosts')
 @click.argument('account')
