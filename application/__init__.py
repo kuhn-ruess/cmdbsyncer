f6912a1116f51277eadb143c025d6accb32cd92f
Bastian Kuhn <mail@bastian-kuhn.de>
2024-03-25 17:27:56 +0100

    Refactor for Checkmk Tag generation

--- application/__init__.py
+++ application/__init__.py
@@ -19,7 +19,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.5.11'
+VERSION = '3.5.12'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
