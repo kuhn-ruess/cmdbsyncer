6b422e5ce8b421fb1d2aedfa388f100202f2532e
Konstantin Büttner <49711311+kbuettner@users.noreply.github.com>
2024-05-10 19:35:10 +0200

    Downtimes (#37)
    * Added Support for Downtime creation

--- application/modules/checkmk/downtimes.py
+++ application/modules/checkmk/downtimes.py
@@ -2,14 +2,68 @@
 Checkmk Downtime Sync
 """
 
+import datetime
 
 from application.modules.checkmk.config_sync import SyncConfiguration
 from syncerapi.v1 import Host, cc
+
+_weekdays = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"]
 
 class CheckmkDowntimeSync(SyncConfiguration):
     """
     Sync Checkmk Downtimes
     """
+
+    def timezone(self):
+        return datetime.timezone.utc #TODO: implement localtime
+
+    def ahead_days(self):
+        today = datetime.date.today()
+        one_day = datetime.timedelta(days=1)
+        return [ today + i * one_day for i in range(14) ]
+
+    def calculate_downtime_days(self, start_day, every):
+        ahead_days = self.ahead_days()
+        if every == "day":
+            return ahead_days
+        elif every == "workday":
+            return [ day for day in ahead_days if day.isoweekday() not in [6, 7] ]
+        elif every == "week":
+            return [ day for day in ahead_days if _weekdays[day.weekday()] == start_day]
+        else:
+            return []
+
+    def calculate_configured_downtimes(self, rule):
+        now = datetime.datetime.now(datetime.timezone.utc)
+        dt_time = datetime.time(hour=rule["start_time_h"],
+                                minute=rule["start_time_m"],
+                                tzinfo=self.timezone())
+        dt_length = datetime.timedelta(hours=rule["duration_h"])
+        for day in self.calculate_downtime_days(rule["start_day"], rule["every"]):
+            dt_start = datetime.datetime.combine(day, dt_time)
+            if dt_start < now:
+                continue
+            dt_end = dt_start + dt_length
+            yield {
+                "start" : dt_start,
+                "end" : dt_end,
+            }
+
+    def set_downtime(self, cmk_site, host, start, end):
+        """
+        host: Hostname as in Checkmk
+        start: Downtime start as a datetime object
+        end: Downtime end as a datetime object
+        """
+        url = "domain-types/downtime/collections/host"
+        data = {
+            "host_name" : host,
+            "downtime_type" : "host",
+            "comment" : "Set by cmdbsyncer",
+            "start_time" : start.isoformat(timespec='seconds'),
+            "end_time" : end.isoformat(timespec='seconds'),
+        }
+        self.request(url, method="POST", data=data)
 
     def export_downtimes(self):
         """
@@ -32,22 +86,43 @@ class CheckmkDowntimeSync(SyncConfiguration):
                 if not 'cmk__label_site' in attributes['all']:
                     print(f"{cc.WARNING} *{cc.ENDC} Host has no cmk Site info")
                     continue
+                # This Attribute needs to be inventorized from Checkmk
+                cmk_site = attributes['all']['cmk__label_site']
 
+                configured_downtimes = []
                 for _rule_type, rules in host_actions.items():
-                    print(rules)
+                    for rule in rules:
+                        if rule["every"] in [ "onces", "2nd_week" ]:
+                            print(f"{cc.WARNING} *{cc.ENDC} Not implemented, need absolute starting point for this to work properly")
+                            continue
+                        configured_downtimes += list(self.calculate_configured_downtimes(rule))
 
-                self.get_current_cmk_downtimes(db_host.hostname, attributes['all'])
+                current_downtimes = list(
+                        self.get_current_cmk_downtimes(cmk_site, db_host.hostname)
+                        )
+
+                for downtime in configured_downtimes:
+                    for existing_dt in current_downtimes:
+                        if not existing_dt["start"] - downtime["start"]:
+                            if not existing_dt["end"] - downtime["end"]:
+                                continue
+                    self.set_downtime(cmk_site, db_host.hostname, downtime["start"], downtime["end"])
 
 
 
-    def get_current_cmk_downtimes(self, hostname, attributes):
+    def get_current_cmk_downtimes(self, cmk_site, hostname):
         """
         Read Downtimes from Checkmk
         """
 
-        # This Attribute needs to be inventorized from Checkmk
-        cmk_site = attributes['cmk__label_site']
         url = f"domain-types/downtime/collections/all?"\
               f"host_name={hostname}&downtime_type=host&site_id={cmk_site}"
         response = self.request(url, method="GET")
-        print(response[0]['value'])
+        downtimes = response[0]['value']
+        for downtime in downtimes:
+            yield {
+                "start" : datetime.datetime.fromisoformat(
+                        downtime["extensions"]["start_time"]),
+                "end" : datetime.datetime.fromisoformat(
+                        downtime["extensions"]["end_time"]),
+            }
