ed57a39caba7ec48ecd12dc255278fac8b348e89
Bastian Kuhn <mail@bastian-kuhn.de>
2024-05-14 15:55:06 +0200

    CMK; New Switch for big Environments, get Hosts by Folder for sync

--- application/__init__.py
+++ application/__init__.py
@@ -17,7 +17,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.6.0'
+VERSION = '3.6.1'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
