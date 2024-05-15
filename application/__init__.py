ed2276aefa50df5bf61c041dbd2e3f75a2ad325c
Bastian Kuhn <mail@bastian-kuhn.de>
2024-05-15 12:06:10 +0200

    CMK: Optimized Folder creations, added collect bulk operations mode for larger environments

--- application/__init__.py
+++ application/__init__.py
@@ -17,7 +17,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.6.1'
+VERSION = '3.6.2'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
