c4d3c1be0300645d2f4f36d63309874cb8711aa4
Bastian Kuhn <mail@bastian-kuhn.de>
2024-06-05 09:15:09 +0200

    Inventorize Attributes: Values are alwaus converted to string to match normal Attributes behavior

--- application/__init__.py
+++ application/__init__.py
@@ -17,7 +17,7 @@ from flask_mongoengine import MongoEngine
 from flask_admin.contrib.fileadmin import FileAdmin
 
 
-VERSION = '3.7b1'
+VERSION = '3.7b1.1'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
