900a62ffc8d9b932c30a7e9e7bcb7a08b95bb71a
Bastian Kuhn <mail@bastian-kuhn.de>
2024-07-19 15:37:35 +0200

    Feat: CMK Jinja Function: cleanup_hostname

--- application/modules/checkmk/helpers.py
+++ application/modules/checkmk/helpers.py
@@ -9,3 +9,10 @@ def cmk_cleanup_tag_id(input_str):
     of strings you wan't to use as tag_id in cmk
     """
     return re.sub('[^a-zA-Z0-9_-]', '_', input_str.strip()).lower()
+
+def cmk_cleanup_hostname(input_str):
+    """
+    Cleans Invalid Chars out
+    of strings you wan't to use as tag_id in cmk
+    """
+    return re.sub('[^a-zA-Z0-9_-]', '_', input_str.strip()).lower()
