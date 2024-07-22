8512f24ba60e74879fe4efbcb42e595753249ad9
Bastian Kuhn <mail@bastian-kuhn.de>
2024-07-22 16:38:18 +0200

    Fixed Debug Icon

--- application/modules/checkmk/helpers.py
+++ application/modules/checkmk/helpers.py
@@ -10,9 +10,19 @@ def cmk_cleanup_tag_id(input_str):
     """
     return re.sub('[^a-zA-Z0-9_-]', '_', input_str.strip()).lower()
 
+def cmk_cleanup_tag_value(input_str):
+    """
+    Cleans invalid Chars in Label/ Tag Values
+    """
+    return re.sub('[^a-zA-Z0-9_-]', '_', input_str.strip()).lower()
+
+
 def cmk_cleanup_hostname(input_str):
     """
     Cleans Invalid Chars out
     of strings you wan't to use as tag_id in cmk
     """
     return re.sub('[^a-zA-Z0-9_-]', '_', input_str.strip()).lower()
+
+
+
