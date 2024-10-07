5cef0d0de9604935e3108b193c24f287c330e90d
Bastian Kuhn <mail@bastian-kuhn.de>
2024-10-07 15:43:03 +0200

    Refactored Rewrite Module

--- application/modules/rule/rewrite.py
+++ application/modules/rule/rewrite.py
@@ -2,96 +2,69 @@
 """
 Rewrite
 """
-#pylint: disable=logging-fstring-interpolation, bare-except, too-many-branches, too-many-locals
-import re
+#pylint: disable=logging-fstring-interpolation
 from application.modules.rule.rule import Rule
 from application import logger
 from application.helpers.syncer_jinja import render_jinja
 
-class Rewrite(Rule):# pylint: disable=too-few-public-methods
+class Rewrite(Rule):
     """
     Rewrite Attributes
     """
 
     name = "Rewrite"
-    regex_cache = {}
+
+    def get_attribute_name(self, outcome):
+        """
+        Get Old and New Attribute Name
+        """
+        old_name = outcome['old_attribute_name']
+        new_name = False
+        if mode := outcome['overwrite_name']:
+            # Default in case of String:
+            new_name = outcome['new_attribute_name']
+            if mode == 'jinja':
+                new_name = render_jinja(new_name, mode="nullify",
+                                        HOSTNAME=self.hostname, **self.attributes)
+        return old_name, new_name
+
+    def get_new_attribute_value(self, outcome, attribute_name):
+        """
+        Rewrite the Value
+        """
+        old_value = self.attributes.get(attribute_name, "")
+        # The old Value stays the new Value, if not overwritten
+        new_value = old_value
+        if value_mode := outcome['overwrite_value']:
+            # Default ijn case of String
+            new_value = outcome['new_value']
+            if value_mode == 'split':
+                what, index = new_value.split(':')
+                try:
+                    splited = old_value.split(what)
+                    new_value = splited[int(index)]
+                except (IndexError, TypeError):
+                    logger.debug("Cant Split Value, old one returned")
+                    return old_value
+            elif value_mode == 'jinja':
+                new_value = render_jinja(new_value, mode="nullify",
+                                         HOSTNAME=self.hostname, **self.attributes)
+        return new_value
 
     def add_outcomes(self, rule, outcomes):
         """
         Rewrite matching Attribute
         """
-        # pylint: disable=too-many-nested-blocks, too-many-statements
         for outcome in rule:
-            attribute_name = outcome['old_attribute_name']
-            # Save inital Attribute value too have it even the attribute name changes
-            old_value = self.attributes.get(attribute_name, "")
-            new_attribute_name = False
-            if mode := outcome['overwrite_name']:
-                new_attribute_name = outcome['new_attribute_name']
-                if mode == 'regex':
-                    pattern = attribute_name
-                    # Can may be simplified since i heared Python
-                    # Handles the cache anyway
-                    if not self.regex_cache.get(pattern):
-                        self.regex_cache[pattern] = re.compile(pattern)
-                    for key, value in self.attributes.items():
-                        if self.regex_cache[pattern].match(key):
-                            outcomes[f'add_{new_attribute_name}'] = value
-                            outcomes[f'del_{key}'] = True
-                elif mode == 'string':
-                    if self.attributes.get(attribute_name):
-                        outcomes[f'add_{new_attribute_name}'] = self.attributes[attribute_name]
-                        outcomes[f'del_{attribute_name}'] = True
-                elif mode == 'split':
-                    if self.attributes.get(attribute_name):
-                        what, index = new_attribute_name.split(':')
-                        try:
-                            new_attribute_name = new_attribute_name.split(what)[int(index)]
-                            outcomes[f'add_{new_attribute_name}'] = self.attributes[attribute_name]
-                            outcomes[f'del_{attribute_name}'] = True
-                        except:
-                            logger.debug("Cant Split Rewrite Attribute")
-                elif mode == 'jinja':
-                    new_attribute_name = render_jinja(new_attribute_name, mode="nullify",
-                                                      HOSTNAME=self.hostname, **self.attributes)
-                    if self.attributes.get(attribute_name):
-                        outcomes[f'add_{new_attribute_name}'] = self.attributes[attribute_name]
-                        outcomes[f'del_{attribute_name}'] = True
-                    else:
-                        # This can happen when we create a complete new one,
-                        # there is now old value, and tis is correctly set in the
-                        # Overwrite Value part
-                        outcomes[f'add_{new_attribute_name}'] = None
+            old_name, current_name = self.get_attribute_name(outcome)
+            if current_name:
+                outcomes[f'del_{old_name}'] = True
+            else:
+                # If we have no new name, the old stays as current one
+                current_name = old_name
+            # We pass the old name, since the attributes store of the host hast the value
+            # stilles stored under this old name
+            new_value = self.get_new_attribute_value(outcome, old_name)
+            outcome[f'add_{current_name}'] = new_value
 
-
-            if value_mode := outcome['overwrite_value']:
-                if new_attribute_name:
-                    # if overwriten before, write overwrite
-                    attribute_name = new_attribute_name
-                if 'new_value' not in outcome:
-                    continue
-                new_value = outcome['new_value']
-
-                if value_mode == 'regex':
-                    pattern = new_value
-                    # Can may be simplified since i heared Python
-                    # Handles the cache anyway
-                    if not self.regex_cache.get(pattern):
-                        self.regex_cache[pattern] = re.compile(pattern)
-                    for key, value in self.attributes.items():
-                        if self.regex_cache[pattern].match(key):
-                            outcomes[f'add_{attribute_name}'] = value
-                elif value_mode == 'string':
-                    outcomes[f'add_{attribute_name}'] = new_value
-                elif value_mode == 'split':
-                    what, index = new_value.split(':')
-                    try:
-                        splited = old_value.split(what)
-                        outcomes[f'add_{attribute_name}'] = splited[int(index)]
-                    except:
-                        logger.debug("Cant Split Value")
-                elif value_mode == 'jinja':
-                    new_value = render_jinja(new_value, mode="nullify",
-                                             HOSTNAME=self.hostname, **self.attributes)
-                    outcomes[f'add_{attribute_name}'] = new_value
-        return outcomes
+        return self.outcomes
