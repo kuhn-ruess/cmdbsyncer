420f317388c87f8183934ea6c27b0087f9efb475
Bastian Kuhn <mail@bastian-kuhn.de>
2024-10-10 19:59:03 +0200

    Updated Account Documentation

--- application/docu_links.py
+++ application/docu_links.py
@@ -2,4 +2,5 @@
 
 docu_links = {
     'rewrite' : 'https://cmdbsyncer.readthedocs.io/en/latest/basics/rewrite_attributes/',
+    'accounts' : 'https://cmdbsyncer.readthedocs.io/en/latest/basics/accounts/',
 }
