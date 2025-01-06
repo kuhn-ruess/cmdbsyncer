838c737c7738dd7f4d810d498fcadcc044034700
Bastian Kuhn <mail@bastian-kuhn.de>
2025-01-06 14:32:35 +0100

    Fix: Netbox: Cleanup of functions and names

--- application/modules/netbox/virtualmachines.py
+++ application/modules/netbox/virtualmachines.py
@@ -7,7 +7,10 @@ from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn, MofNComple
 from application.modules.netbox.netbox import SyncNetbox
 from application import logger
 
-from application.models.host import Host
+from syncerapi.v1 import (
+    cc,
+    Host,
+)
 
 
 class SyncVirtualMachines(SyncNetbox):
@@ -105,3 +108,16 @@ class SyncVirtualMachines(SyncNetbox):
 
                 progress.advance(task1)
 #.
+    def import_hosts(self):
+        for vm in self.nb.virtualization.virtual_machines.all():
+            hostname = vm.name
+            labels = vm.__dict__
+            if 'rewrite_hostname' in self.config and self.config['rewrite_hostname']:
+                hostname = Host.rewrite_hostname(hostname, self.config['rewrite_hostname'], labels)
+            host_obj = Host.get_host(hostname)
+            print(f"\n{cc.HEADER}Process VM: {hostname}{cc.ENDC}")
+            host_obj.update_host(labels)
+            do_save = host_obj.update_host(labels)
+            do_save = host_obj.set_account(account_dict=self.config)
+            if do_save:
+                host_obj.save()
