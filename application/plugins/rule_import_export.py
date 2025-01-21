96fc65b55af93b2e40db9f5c348e6a0f15bfea3e
Bastian Kuhn <mail@bastian-kuhn.de>
2025-01-21 11:49:27 +0100

    Fix: Import of rules exported from GUI no longer skip first line

--- application/plugins/rule_import_export.py
+++ application/plugins/rule_import_export.py
@@ -85,12 +85,28 @@ def export_rules(rule_type):
         for rulename in sorted(enabled_rules):
             print(rulename)
 
+
+def import_line(json_dict, model, rule_type):
+    """
+    Import a Single line
+    """
+    print(f"* Import {json_dict['_id']}")
+    db_ref = getattr(model, enabled_rules[rule_type][1])()
+    new = db_ref.from_json(json.dumps(json_dict))
+    try:
+        new.save(force_insert=True)
+    except NotUniqueError:
+        print("   Already existed")
+    except ValidationError:
+        print(f"Problem with entry: {json_dict}")
+
 @cli_rules.command('import_rules')
 @click.argument("rulefile_path")
 def import_rules(rulefile_path):
     """
     Import Rules into the CMDB Syncer
     """
+    import_firstline = False
     with open(rulefile_path, encoding='utf-8') as rulefile:
         rule_type = False
         for line in rulefile.readlines():
@@ -106,18 +122,14 @@ def import_rules(rulefile_path):
                     # Mode get type by filename
                     # This mode is for export from GUI
                     rule_type = get_ruletype_by_filename(rulefile_path)
+                    import_firstline = True
                 if rule_type not in enabled_rules:
                     print("Ruletype not supported")
                     print(f"Currently supported: {', '.join(enabled_rules.keys())}")
                     return
                 model = importlib.import_module(enabled_rules[rule_type][0])
+                if import_firstline:
+                    # This is the case for imports where we guess the type
+                    import_line(json_dict, model, rule_type)
             else:
-                print(f"* Import {json_dict['_id']}")
-                db_ref = getattr(model, enabled_rules[rule_type][1])()
-                new = db_ref.from_json(json.dumps(json_dict))
-                try:
-                    new.save(force_insert=True)
-                except NotUniqueError:
-                    print("   Already existed")
-                except ValidationError:
-                    print(f"Problem with entry: {json_dict}")
+                import_line(json_dict, model, rule_type)
