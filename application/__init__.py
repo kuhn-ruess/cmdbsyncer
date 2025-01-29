7be52c4a1232216a92c00638503f2a4b8370c75c
Bastian Kuhn <mail@bastian-kuhn.de>
2025-01-29 11:07:34 +0100

    Fix: Raise the correct error message if Hostnames are not of type string

--- application/__init__.py
+++ application/__init__.py
@@ -25,7 +25,7 @@ from tablib.formats import registry as tablib_registry
 tablib_registry.register('syncer_rules', ExportObjects())
 
 
-VERSION = '3.8 Daily 28.01.2025'
+VERSION = '3.8 Daily 29.01.2025'
 # create logger
 logger = logging.getLogger('cmdb_syncer')
 
