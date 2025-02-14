cfc860e798b03ef6c77ad5d0999eaf3a5894c501
Bastian Kuhn <mail@bastian-kuhn.de>
2025-02-14 15:04:50 +0100

    Fix 2 for serial

--- application/modules/netbox/rules.py
+++ application/modules/netbox/rules.py
@@ -56,11 +56,11 @@ class NetboxVariableRule(Rule):# pylint: disable=too-few-public-methods
             else:
 
                 new_value  = render_jinja(action_param, mode="nullify", **self.attributes)
+                new_value = prepare_value(new_value)
                 if field == 'serial':
-                    if new_value == 'None' or not new_value:
+                    if not new_value:
                         new_value = 'Unknown'
                     new_value = new_value[:50]
-                new_value = prepare_value(new_value)
 
                 if field in sub_values:
                     outcomes['sub_fields'][field] = {'value': new_value}
