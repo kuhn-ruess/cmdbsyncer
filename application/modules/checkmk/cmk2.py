a9b8008c0795ccf05fa6b88ff95d0e940fbdfbc1
Bastian Kuhn <mail@bastian-kuhn.de>
2025-02-20 14:08:22 +0100

    Feat: Checkmk Inventorize now supports 'Get hosts by folder'

--- application/modules/checkmk/cmk2.py
+++ application/modules/checkmk/cmk2.py
@@ -2,7 +2,9 @@
 Central Request Modul to CMK 2.x
 """
 #pylint: disable=logging-fstring-interpolation
+import multiprocessing
 import requests
+from rich.progress import Progress, SpinnerColumn, TimeElapsedColumn, MofNCompleteColumn
 #from requests.exceptions import ConnectionError
 from application import app
 from application.modules.plugin import Plugin
@@ -26,6 +28,12 @@ class CMK2(Plugin):
     """
 
     checkmk_version = False
+
+    checkmk_hosts = {}
+
+    existing_folders = []
+    existing_folders_attributes = {}
+    custom_folder_attributes = {}
 
 
     def __init__(self, account=False):
@@ -102,3 +110,76 @@ class CMK2(Plugin):
         except ConnectionError:
             #pylint: disable=raise-missing-from
             raise CmkException("Can't connect to Checkmk")
+
+    def fetch_checkmk_folders(self):
+        """
+        Fetch list of Folders in Checkmk
+        """
+        url = "domain-types/folder_config/collections/all"
+        url += "?parent=/&recursive=true&show_hosts=false"
+        with Progress(SpinnerColumn(),
+                      MofNCompleteColumn(),
+                      *Progress.get_default_columns(),
+                      TimeElapsedColumn()) as progress:
+            task1 = progress.add_task("Fetching Current Folders", start=False)
+            api_folders = self.request(url, method="GET")
+            if not api_folders[0]:
+                raise CmkException("Cant connect or auth with CMK")
+            progress.update(task1, total=len(api_folders[0]['value']), start=True)
+            for folder in api_folders[0]['value']:
+                progress.update(task1, advance=1)
+                path = folder['extensions']['path']
+                attributes = folder['extensions']['attributes']
+                self.existing_folders_attributes[path] = attributes
+                self.existing_folders_attributes[path].update({'title': folder['title']})
+                self.existing_folders.append(path)
+
+    def fetch_all_checkmk_hosts(self, extra_params=""):
+        """
+        Classic full Fetch
+        """
+        url = f"domain-types/host_config/collections/all{extra_params}"
+        with Progress(SpinnerColumn(),
+                      MofNCompleteColumn(),
+                      *Progress.get_default_columns(),
+                      TimeElapsedColumn()) as progress:
+            task1 = progress.add_task("Fetching Hosts", start=False)
+            progress.console.print("Waiting for Checkmk Response")
+            api_hosts = self.request(url, method="GET")
+            progress.update(task1, total=len(api_hosts[0]['value']), start=True)
+            for host in api_hosts[0]['value']:
+                self.checkmk_hosts[host['id']] = host
+                progress.update(task1, advance=1)
+
+
+    def get_hosts_of_folder(self, folder, return_dict, extra_params):
+        """ Get Hosts of given folder """
+        folder = folder.replace('/','~')
+        url = f"objects/folder_config/{folder}/collections/hosts{extra_params}"
+        api_hosts = self.request(url, method="GET")
+        for host in api_hosts[0]['value']:
+            return_dict[host['id']] = host
+
+    def _fetch_checkmk_host_by_folder(self, extra_params=""):
+        """
+        Check the folder Structure and get hosts
+        whit multiple request
+        """
+        with Progress(SpinnerColumn(),
+                      MofNCompleteColumn(),
+                      *Progress.get_default_columns(),
+                      TimeElapsedColumn()) as progress:
+            num_folders = len(self.existing_folders)
+
+            task1 = progress.add_task("Fetching Hosts folder by folder", total=num_folders)
+            manager = multiprocessing.Manager()
+            return_dict = manager.dict()
+            with multiprocessing.Pool() as pool:
+                for folder in self.existing_folders:
+                    pool.apply_async(self.get_hosts_of_folder,
+                                     args=(folder, return_dict, extra_params,),
+                                     callback=lambda x: progress.advance(task1))
+
+                pool.close()
+                pool.join()
+                self.checkmk_hosts.update(return_dict)
