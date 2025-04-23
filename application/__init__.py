93f2442805179f239a5d4ed9e97629f1bde6c79f
Bastian Kuhn <mail@bastian-kuhn.de>
2025-04-23 17:25:04 +0200

    Experimental Changes for flask_admin 2 support

--- application/__init__.py
+++ application/__init__.py
@@ -28,7 +28,7 @@ import mongoengine
 tablib_registry.register('syncer_rules', ExportObjects())
 
 
-VERSION = '3.9.0-dev2'
+VERSION = '3.9.0-mongoengine'
 
 # create logger
 
@@ -159,7 +159,7 @@ from application.api.views import API_BP as api
 app.register_blueprint(api, url_prefix="/api/v1")
 
 admin = Admin(app, name=f"CMDB Syncer {VERSION} {app.config['HEADER_HINT']}",
-                   template_mode='bootstrap4', index_view=IndexView(),
+                   index_view=IndexView(),
                    category_icon_classes={
                        })
 
