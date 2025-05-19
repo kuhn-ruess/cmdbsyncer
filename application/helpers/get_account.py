73e459e768968b19001f4dfc042473a2a43b3921
Bastian Kuhn <mail@bastian-kuhn.de>
2025-05-19 13:16:47 +0200

    Fix: Fixed Child accounts feature

--- application/helpers/get_account.py
+++ application/helpers/get_account.py
@@ -25,7 +25,7 @@ def get_account_by_name(name, is_id=False):
             account = Account.objects.get(name=name, enabled=True)
 
         if account.is_child:
-            account_data = dict(get_account_by_name(account['parent'], is_id=True).to_mongo())
+            account_data = dict(get_account_by_name(account['parent'].id, is_id=True))
             account_data['is_child'] = True
         else:
             account_data = dict(account.to_mongo())
