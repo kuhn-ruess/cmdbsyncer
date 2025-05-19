f3e904a099fca746af9ec1ecc867396d99d299e9
Bastian Kuhn <mail@bastian-kuhn.de>
2025-05-19 18:12:10 +0200

    Updated Docu Links

--- application/docu_links.py
+++ application/docu_links.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
 docu_links = {
-    'rewrite' : 'https://cmdbsyncer.readthedocs.io/en/latest/basics/rewrite_attributes/',
-    'accounts' : 'https://cmdbsyncer.readthedocs.io/en/latest/basics/accounts/',
-    'cmk_inventory_attributes': 'https://cmdbsyncer.readthedocs.io/en/latest/checkmk/inventorize/',
-    'cmk_groups': 'https://cmdbsyncer.readthedocs.io/en/latest/checkmk/groups_management/',
-    'cmk_setup_rules': 'https://cmdbsyncer.readthedocs.io/en/latest/checkmk/rules_management/',
-    'cmk_hosttags': 'https://cmdbsyncer.readthedocs.io/en/latest/checkmk/create_hosttags/',
-    'cmk_password_store': 'https://cmdbsyncer.readthedocs.io/en/latest/checkmk/password_store/',
+    'rewrite' : 'https://docs.cmdbsyncer.de/basics/rewrite_attributes/',
+    'accounts' : 'https://docs.cmdbsyncer.de/basics/accounts/',
+    'cmk_inventory_attributes': 'https://docs.cmdbsyncer.de/checkmk/inventorize/',
+    'cmk_groups': 'https://docs.cmdbsyncer.de/checkmk/groups_management/',
+    'cmk_setup_rules': 'https://docs.cmdbsyncer.de/checkmk/rules_management/',
+    'cmk_hosttags': 'https://docs.cmdbsyncer.de/checkmk/create_hosttags/',
+    'cmk_password_store': 'https://docs.cmdbsyncer.de/checkmk/password_store/',
 }
