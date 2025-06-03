0badc7e9c00ee4d2a1cd7c930dbce3f3595a571d
Sven Rueß <github@sven-ruess.de>
2025-06-03 23:39:41 +0200

    Only delete existing keys in dict
    Signed-off-by: Sven Rueß <github@sven-ruess.de>

--- application/helpers/get_account.py
+++ application/helpers/get_account.py
@@ -18,21 +18,24 @@ def get_account_by_name(name, is_id=False):
 
     parent_account = {}
     try:
-
         if is_id:
             account = Account.objects.get(id=name, enabled=True)
         else:
             account = Account.objects.get(name=name, enabled=True)
 
         account_data = dict(account.to_mongo())
+
         if account.is_child:
             parent_account = get_account_by_name(account.parent.id, is_id=True)
             account_data['is_child'] = True
+
             # make sure to not use the following fields from the child
             for what in ['typ', 'username', 'password', 'address',
                          'object_type',
                         ]:
-                del account_data[what]
+                if what in account_data:
+                    del account_data[what]
+
         else:
             account_data['password'] = account.get_password()
 
@@ -40,6 +43,7 @@ def get_account_by_name(name, is_id=False):
             if not value:
                 value = False
             account_data[field] = value
+
         account_data['settings'] = {}
         for plugin, object_filter  in [(x['plugin'],
                                         x.get('object_filter'))
@@ -47,20 +51,23 @@ def get_account_by_name(name, is_id=False):
             account_data['settings'][plugin] = {}
             account_data['settings'][plugin]['filter'] = object_filter
 
-
         account_data['id'] = str(account_data['_id'])
         account_data['ref'] = account_data['_id']
+
         if parent_account:
             # A Parent Account is the base, but the child
             # overwrites it. Account_data is in this case the child
             parent_account.update(account_data)
             account_data = parent_account
+
         # cleanup 2
         for what in ['custom_fields', 'password_crypted',
                      'plugin_settings']:
             if what in account:
                 del account_data[what]
+
         return account_data
+
     except DoesNotExist as exc:
         raise AccountNotFoundError("Account not found") from exc
 
