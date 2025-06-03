6957e100e7797aaf4546e528972b03ca590a4084
Bastian Kuhn <mail@bastian-kuhn.de>
2025-06-03 19:45:13 +0200

    Fix: Child Account now merges correctly

--- application/modules/checkmk/cmk2.py
+++ application/modules/checkmk/cmk2.py
@@ -42,7 +42,7 @@ class CMK2(Plugin):
         """
         super().__init__(account)
 
-        if account and not self.checkmk_version:
+        if self.config and not self.checkmk_version:
             data = self.request('/version')[0]
             self.checkmk_version = data['versions']['checkmk']
 
