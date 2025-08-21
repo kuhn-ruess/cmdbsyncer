4b8b726eed7a23d31a034f1bd4c6109514d82543
Bastian Kuhn <mail@bastian-kuhn.de>
2025-08-21 09:59:55 +0200

    Fix user model to enforce unique names and add migration helper for missing names

--- application/api/__init__.py
+++ application/api/__init__.py
@@ -5,6 +5,7 @@ from functools import wraps
 from flask import abort, request
 from application.models.account import Account
 from application.models.user import User
+from mongoengine.errors import DoesNotExist
 
 def require_token(fn): #pylint: disable=invalid-name
     """
@@ -14,7 +15,13 @@ def require_token(fn): #pylint: disable=invalid-name
     def decorated_view(*args, **kwargs):
         if login_user := request.headers.get('x-login-user'):
             username, user_password = login_user.split(':', 1)
-            user_result = User.objects.get(email=username, disabled__ne=True)
+            try:
+                user_result = User.objects.get(
+                    disabled__ne=True,
+                    __raw__={'$or': [{'name': username}, {'email': username}]}
+                )
+            except DoesNotExist:
+                abort(401, "Invalid login")
             if not user_result.check_password(user_password):
                 abort(401, "Invalid login")
         elif request.headers.get('x-login-token'):
