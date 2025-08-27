74ada89e2aecd090570214c2db4b7344fc9db340
Bastian Kuhn <mail@bastian-kuhn.de>
2025-08-27 18:56:07 +0200

    Feat: Improved FileAdmin

---
+++ application/views/fileadmin.py
@@ -0,0 +1,24 @@
+from flask_login import current_user
+from flask_admin.contrib.fileadmin import FileAdmin
+
+
+class FileAdminView(FileAdmin):
+    """
+    Fileadmin Settings
+    """
+
+
+    can_rename = True
+    rename_modal = True
+    upload_modal = True
+
+    mkdir_modal = True
+    edit_modal = True
+
+
+    allowed_extensions = ('md', 'txt', 'csv', 'yml', 'json')
+    editable_extensions = ('md', 'txt', 'csv', 'yml', 'json')
+
+    def is_accessible(self):
+        """ Overwrite """
+        return current_user.is_authenticated and current_user.has_right('fileadmin')
