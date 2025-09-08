f07768d5f4836c591b7b51b4c441813ad5df5e3a
Bastian Kuhn <mail@bastian-kuhn.de>
2025-09-08 18:59:35 +0200

    Fix: Checkmk Group Creation: Better detect already existing groups

--- application/__init__.py
+++ application/__init__.py
@@ -23,7 +23,7 @@ from application.helpers.tablib_formater import ExportObjects
 
 tablib_registry.register('syncer_rules', ExportObjects())
 
-VERSION = '3.10.6'
+VERSION = '3.10.7'
 
 
 app = Flask(__name__)
