d52c527abafed96f4c4fea7d272594bc1bb2a693
Bastian Kuhn <mail@bastian-kuhn.de>
2025-09-12 14:10:09 +0200

    Feat: Add debug option to MSSQL and ODBC import and inventorize commands

--- application/plugins/pyodbc.py
+++ application/plugins/pyodbc.py
@@ -105,37 +105,41 @@ class ODBC(Plugin):
 def cli_odbc():
     """ODBC commands"""
 
-def odbc_import(account):
+def odbc_import(account, debug=False):
     """
     ODBC Inner Import
     """
     odbc = ODBC(account)
     odbc.name = f"Import data from {account}"
     odbc.source = "odbc_import"
+    odbc.debug = debug
     odbc.sql_import()
 
 @cli_odbc.command('import_hosts')
+@click.option("--debug", default=False, is_flag=True)
 @click.argument('account')
-def cli_odbc_import(account):
+def cli_odbc_import(account, debug):
     """Import ODBC Hosts"""
-    odbc_import(account)
+    odbc_import(account, debug)
 
 
-def odbc_inventorize(account):
+def odbc_inventorize(account, debug=False):
     """
     ODBC Inner Inventorize
     """
     odbc = ODBC(account)
     odbc.name = f"Inventorize data from {account}"
     odbc.source = "odbc_inventorize"
+    odbc.debug = debug
     odbc.sql_inventorize()
 
 
 @cli_odbc.command('inventorize_hosts')
+@click.option("--debug", default=False, is_flag=True)
 @click.argument('account')
-def cli_odbc_inventorize(account):
+def cli_odbc_inventorize(account, debug):
     """Inventorize ODBC Data"""
-    odbc_inventorize(account)
+    odbc_inventorize(account, debug)
 
 register_cronjob("ODBC: Import Hosts", odbc_import)
 register_cronjob("ODBC: Inventorize Data", odbc_inventorize)
