a15d6782d5c6e2928f8736fa26ef4654dcc87bb6
Bastian Kuhn <mail@bastian-kuhn.de>
2025-09-12 13:27:28 +0200

    Feat: Add debug option to MSSQL import and inventorize commands

--- application/__init__.py
+++ application/__init__.py
@@ -23,7 +23,7 @@ from application.helpers.tablib_formater import ExportObjects
 
 tablib_registry.register('syncer_rules', ExportObjects())
 
-VERSION = '3.10.7'
+VERSION = '3.10.8'
 
 
 app = Flask(__name__)
