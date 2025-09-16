64f8d2c05af4b77e48821fe0223e78db6f94bb09
Bastian Kuhn <mail@bastian-kuhn.de>
2025-09-16 16:41:01 +0200

    Fix: Checkmk Downtimes: Fields now support again (jinja) strings instead of beeing a Dropdown

--- application/__init__.py
+++ application/__init__.py
@@ -23,7 +23,7 @@ from application.helpers.tablib_formater import ExportObjects
 
 tablib_registry.register('syncer_rules', ExportObjects())
 
-VERSION = '3.10.8'
+VERSION = '3.10.9'
 
 
 app = Flask(__name__)
