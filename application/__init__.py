c47b3ad885845100ef2aeab74b059a1605e7f031
Bastian Kuhn <mail@bastian-kuhn.de>
2026-01-13 10:29:44 +0100

    Updated Version

--- application/__init__.py
+++ application/__init__.py
@@ -27,7 +27,7 @@ warnings.filterwarnings('ignore', category=UserWarning)
 
 tablib_registry.register('syncer_rules', ExportObjects())
 
-VERSION = '3.11.1'
+VERSION = '3.11.2'
 
 app = Flask(__name__)
 env = os.environ.get('config')
