0f231b5a4a82e4894d2b24a55b2d59bd71858231
Bastian Kuhn <mail@bastian-kuhn.de>
2026-02-04 10:16:04 +0100

    Feat: Implement rule auto-creation

--- application/plugins/rules/models.py
+++ application/plugins/rules/models.py
@@ -1,6 +1,6 @@
 from application import db, app
 from application.models.account import object_types
-from .rule_definitions import rules as rule_names
+from .rule_definitions import rule_names
 
 class SyncerRuleAutomation(db.Document):
     """
@@ -8,7 +8,6 @@ class SyncerRuleAutomation(db.Document):
     """
     name = db.StringField(required=True, unique=True, max_length=255)
     documentation = db.StringField()
-
     rule_type = db.StringField(choices=rule_names)
     rule_body = db.StringField(required=True)
 
