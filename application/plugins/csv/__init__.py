fcd77887d0444a95454c0cdac906d8676a50f911
Bastian Kuhn <mail@bastian-kuhn.de>
2026-02-27 13:34:31 +0100

    Feat:  CSV now has legacy mode to direactly specify a CSV file as in 1.0

--- application/plugins/csv/__init__.py
+++ application/plugins/csv/__init__.py
@@ -9,9 +9,10 @@ def _cli_csv():
     """CSV Import/ Inventorize"""
 
 @_cli_csv.command('inventorize_hosts')
-@click.argument("account")
+@click.argument("account", required=False)
 @click.option("--debug", default=False, is_flag=True)
-def cli_inventorize_hosts(account, debug):
+@click.option("--legacy", default=None, help="Path to CSV file for legacy mode (bypasses account config)")
+def cli_inventorize_hosts(account, debug, legacy):
     """
     Add Inventory Information to hosts
     Source is a CSV. Every other Column then the hostname Column, will translate
@@ -19,15 +20,29 @@ def cli_inventorize_hosts(account, debug):
 
     ### Example
     _./cmdbsyncer csv inventorize_hosts ACCOUNT_
+    _./cmdbsyncer csv inventorize_hosts --legacy /path/to/file.csv_
 
     Args:
         account (string): Name of Account to read config from
+        legacy (string): Path to CSV file for legacy mode
     """
-    inventorize_hosts(account, debug)
+    if legacy and account:
+        click.echo("Error: Cannot use both account and --legacy option")
+        return
+    if not legacy and not account:
+        click.echo("Error: Either account or --legacy option is required")
+        return
+    inventorize_hosts(account, debug, legacy)
 
 
-def inventorize_hosts(account, debug=False):
-    csv = CSV(account)
+def inventorize_hosts(account=None, debug=False, legacy=None):
+    if legacy:
+        csv = CSV()
+        csv.config = _create_legacy_config(legacy)
+        csv.account_name = "legacy"
+        csv.account_id = "legacy"
+    else:
+        csv = CSV(account)
     csv.debug = debug
     csv.inventorize_hosts()
 
@@ -36,9 +51,10 @@ def inventorize_hosts(account, debug=False):
 register_cronjob('CSV: Inventorize Hosts', inventorize_hosts)
 
 @_cli_csv.command('import_hosts')
-@click.argument("account")
+@click.argument("account", required=False)
 @click.option("--debug", default=False, is_flag=True)
-def cli_import_hosts(account, debug):
+@click.option("--legacy", default=None, help="Path to CSV file for legacy mode (bypasses account config)")
+def cli_import_hosts(account, debug, legacy):
     """
     Import Objects from CSV and make File the Master
     Every CSV column, other then the host column, will translate
@@ -48,17 +64,53 @@ def cli_import_hosts(account, debug):
 
     ### Example
     _./cmdbsyncer csv import_hosts ACCOUNT_
-
+    _./cmdbsyncer csv import_hosts --legacy /path/to/file.csv_
 
     Args:
         account (string): Name of Account to read config from
+        legacy (string): Path to CSV file for legacy mode
     """
-    import_hosts(account, debug)
+    if legacy and account:
+        click.echo("Error: Cannot use both account and --legacy option")
+        return
+    if not legacy and not account:
+        click.echo("Error: Either account or --legacy option is required")
+        return
+    import_hosts(account, debug, legacy)
 
-def import_hosts(account, debug=False):
-    csv = CSV(account)
+def import_hosts(account=None, debug=False, legacy=None):
+    if legacy:
+        csv = CSV()
+        csv.config = _create_legacy_config(legacy)
+        csv.account_name = "legacy"
+        csv.account_id = "legacy"
+    else:
+        csv = CSV(account)
     csv.debug = debug
     csv.import_hosts()
+
+def _create_legacy_config(csv_path):
+    """
+    Create a minimal configuration for legacy mode
+    
+    Args:
+        csv_path (string): Path to the CSV file
+        
+    Returns:
+        dict: Minimal configuration dictionary
+    """
+    return {
+        'name': 'legacy_csv',
+        'id': 'legacy_csv',
+        '_id': 'legacy_csv', 
+        'path': csv_path,
+        'encoding': 'utf-8',
+        'delimiter': ';',
+        'hostname_field': 'host',
+        'rewrite_hostname': None,
+        'delete_host_if_not_found_on_import': None,
+        'verify_cert': True
+    }
 
 register_cronjob('CSV: Import Hosts', import_hosts)
 
